f68761b768dc4fb0765b3854f672247bc038db6e
CFHoldren <CFHoldren@student.fullsail.edu>
2022-07-21 22:30:09 -0700

    Created Model/TestModel.cs, made TestData public

---
+++ Codebase/Typerr/Model/TestModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Typerr.Model
+{
+    class TestModel
+    {
+        public Article article { get; private set; }
+        public TestData testData { get; private set; }
+    }
+}
