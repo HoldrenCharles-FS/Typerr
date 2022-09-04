d12be05148897a537fea7be85b5e6b17dc4b34fc
CFHoldren <CFHoldren@student.fullsail.edu>
2022-09-03 20:23:36 -0700

    AddImageCommand implemented

--- Codebase/Typerr/Commands/AddImageCommand.cs
+++ Codebase/Typerr/Commands/AddImageCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Typerr.ViewModel;
@@ -16,7 +17,17 @@ namespace Typerr.Commands
 
         public override void Execute(object parameter)
         {
-            throw new NotImplementedException();
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.tif;";
+            dlg.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            dlg.Title = "Add a cover image";
+
+            bool? result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                _createTestViewModel.Image = new System.Windows.Media.Imaging.BitmapImage(new Uri(dlg.FileName));
+            }
         }
     }
 }
